f90122df4250bf1e606e2204c5639256659901df
davidfernndez775 <davidfernndez775@yahoo.com>
2024-11-13 11:18:56 -0500

    before homewor of minimap

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrl: './mini-map.component.css'
+  styleUrl: './mini-map.component.css',
 })
 export class MiniMapComponent {
+  @Input() lngLat?: [number, number];
 
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement) throw 'Map Div not found';
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    // map
+
+    // marker
+  }
 }
