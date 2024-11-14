6911f316f2a9704d3e7bd081d66e65e560e3af58
davidfernndez775 <davidfernndez775@yahoo.com>
2024-11-14 14:36:16 -0500

    section 22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideMenuComponent } from './alone/components/side-menu/side-menu.component';
 
 const routes: Routes = [
   {
     path: 'maps',
     loadChildren: () => import('./maps/maps.module').then((m) => m.MapsModule),
+  },
+  {
+    // enrutamiento de un standalone component
+    path: 'alone',
+    loadComponent: () =>
+      import('./alone/pages/alone-page/alone-page.component').then(
+        (m) => m.AlonePageComponent
+      ),
   },
   {
     path: '**',
